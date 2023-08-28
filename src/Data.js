d9100b07bbefd35a747d47b36cfc28b726ef1eb9
Loo-ney <noe.ralte@gmail.com>
2023-08-28 15:41:13 +0530

    one commit

---
+++ src/Data.js
@@ -0,0 +1,90 @@
+let shopItemsData = [
+    {
+        id:"ghgdhgj",
+        name: "Cuban Collar",
+        price: 1999,
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        img: "imgs/img-1.webp"  
+    },
+    {
+        id:"hgjhgj",
+        name: "Black Casual",
+        price: 2499,
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        img: "imgs/img-2.webp"  
+    },
+    {
+        id:"bcvnbhjh",
+        name: "Linen Blend",
+        price: 1499,
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        img: "imgs/img-3.jpeg" 
+    },
+    {
+        id:"hgjhkjh",
+        name: "Black Printed",
+        price: 1499,
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        img: "imgs/img-4.webp" 
+    },
+    {
+        id:"jhihn",
+        name: "Hawaiian Shirt",
+        price: 1499,
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        img: "imgs/img-5.webp" 
+    },
+    {   
+        id:"oipioiuo",
+        name: "Black Print",
+        price: 2499,
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        img: "imgs/img-6.webp"
+    },    
+    {   
+        id:"dkfjhijh",
+        name: "Indian print",
+        price: 2499,
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        img: "imgs/img-7.webp"
+    },
+    {   id:"fbhjfg",
+        name: "Casual White",
+        price: 3599,
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        img: "imgs/img-8.webp"
+    }, 
+    {   
+        id:"mamanj",
+        name: "Harringbone",
+        price: 4599,
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        img: "imgs/img-9.webp"
+    },  
+    {   
+        id:"jinkdo",
+        name: "Monk Ankle",
+        price: 6599,
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        img: "imgs/img-10.webp"
+    },  
+    {  
+        id:"njilko",
+        name: "Brogue Oxford",
+        price: 4599,
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        img: "imgs/img-11.webp"
+    },   
+    {   
+        id:"bjdfhjsd",
+        name: "Harringbone Beige",
+        price: 4599,
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        img: "imgs/img-12.webp"
+    },    
+
+] 
+   
+
+
+
